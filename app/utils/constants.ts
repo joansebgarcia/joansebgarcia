36fb34f5f06fe64f19653771bbd5579eab08eaf8
Joan Sebastian García <joangarcia.dev@gmail.com>
2026-07-12 22:21:45 -0500

    feat: update Linkedin profile

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,7 +1,7 @@
 export const GITHU_API_URL = 'https://api.github.com/users/joansebgarcia'
 
 export const SOCIAL_LINKS = {
-  LINKEDIN: 'https://linkedin.com/in/sebastiandotdev',
+  LINKEDIN: 'https://linkedin.com/in/joansebgarcia',
   GITHUB: 'https://github.com/joansebgarcia',
   GMAIL: 'mailto:joangarcia.dev@gmail.com',
 } as const
